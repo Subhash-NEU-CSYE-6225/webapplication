4c3e6271e6e4ec26b1ad8348edd845432db9ae63
Subhash Chandran <shankarakumar.s@northeastern.edu>
2022-11-02 02:18:27 -0400

    Added scripts.sh

---
+++ packer-ami/scripts.sh
@@ -0,0 +1,14 @@
+sleep 30
+
+sudo apt-get install python3
+sudo apt-get update
+sudo apt-get -y install python3-pip
+sudo pip install flask==2
+sudo pip install psycopg==3
+sudo pip install bcrypt==4
+sudo pip install Flask-Bcrypt==1
+sudo pip install requests==2
+sudo pip install flask-sqlalchemy==1
+sudo pip install sqlalchemy==1
+sudo pip install sqlalchemy_utils
+sudo apt-get -y install postgresql postgresql-contrib
